aa13b994afe4bd948f1e1854264f7960410f7ebe
vpresecki <presecki.vedran@gmail.com>
2019-11-24 22:07:21 +0100

    test deploy

--- server/server.js
+++ server/server.js
@@ -1,28 +1,21 @@
 const express = require("express");
 const server = express();
 const cors = require("cors");
-const history = require("connect-history-api-fallback");
 
 const gameRoute = require("./routes/words");
 server.use("/api/trijumf/game", gameRoute);
 
+server.use(cors());
+
 // Handle production
-if (process.env.NODE_ENV === "production") {
-  server.use(express.static(__dirname + "/public/"));
-}
+// if (process.env.NODE_ENV === "production") {
+server.use(express.static(__dirname + "/public"));
+// }
 
 // Handle SPA
 server.get(/.*/, (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
-
-server.use(cors());
-server.use(
-  history({
-    disableDotRule: true,
-    verbose: true
-  })
-);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
