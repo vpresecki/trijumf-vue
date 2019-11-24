255525809230b85e9887db812e5dcd7ceeb83e15
vpresecki <presecki.vedran@gmail.com>
2019-11-24 22:29:39 +0100

    heroku deploy

--- server/server.js
+++ server/server.js
@@ -13,9 +13,9 @@ server.use(express.static(__dirname + "/public"));
 // }
 
 // Handle SPA
-server.get(/.*/, (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
+// server.get(/.*/, (req, res) => {
+//   res.sendFile(__dirname + "/public/index.html");
+// });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
